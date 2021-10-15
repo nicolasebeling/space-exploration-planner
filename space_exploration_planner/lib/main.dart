c5622dcf3098d53b46c5e53d6aa85ca09058ab84
Nicolas Ebeling <70470098+nicolasebeling@users.noreply.github.com>
2021-10-15 11:23:58 +0200

    Move the entire source code to main.dart for simplicity

--- space_exploration_planner/lib/main.dart
+++ space_exploration_planner/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'homepage.dart';
-
 void main() => runApp(const SpaceExplorationPlanner());
 
 class SpaceExplorationPlanner extends StatelessWidget {
@@ -20,5 +18,72 @@ class SpaceExplorationPlanner extends StatelessWidget {
       ),
       home: const Homepage(),
     );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final tasks = [
+    Task(task: 'Build a rocket'),
+    Task(task: 'Test the rocket\'s boosters'),
+    Task(task: 'Fly to the moon'),
+    Task(task: 'Come back to earth'),
+    Task(task: 'Visit Mars'),
+  ];
+
+  int countChecked() {
+    int count = 0;
+    for (Task t in tasks) {
+      if (t.isSelected) count++;
+    }
+    return count;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Space Exploration Planner'),
+      ),
+      body: Column(
+        children: [
+          LinearProgressIndicator(
+            value: countChecked() / tasks.length,
+            backgroundColor: Theme.of(context).primaryColor.withOpacity(0.5),
+            minHeight: 10,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: tasks.length,
+            itemBuilder: (context, index) => CheckboxListTile(
+              value: tasks[index].isSelected,
+              onChanged: (_) => setState(
+                () => tasks[index].select(),
+              ),
+              title: Text(
+                tasks[index].task,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Task {
+  String task;
+  bool isSelected = false;
+
+  Task({required this.task});
+
+  select() {
+    isSelected = !isSelected;
   }
 }
